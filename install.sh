14c314f9583487a3db3dfc7ebe4ed334dc01e628
Stefan Beck <13620229+beckjim@users.noreply.github.com>
2022-03-30 16:29:37 +0200

    savepoint

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ echo downloading latest awscliv2 zip package
 curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
 sudo ./aws/install --update
+
+
 rm -rf $TMPDIR
 cd $OLDPWD
 
+echo downloading latest eksctl package
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
